f692a8a74171f33a4b4e78c952631fe6d6fb97e5
THIJS-LAPTOP\thijs <thijsvanderwall@live.nl>
2018-12-19 11:56:02 +0100

    Responsive vieuwport (test)

--- StartPage.php
+++ StartPage.php
@@ -7,6 +7,7 @@ STARTPAGE WEBAPP
     <head>
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Start Pagina</title>
     </head>
     <div id="mainContainer">

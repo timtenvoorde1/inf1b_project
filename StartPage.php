94c83cfa01e287d81f1c9bc92ba32c6daab93401
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2019-01-07 11:15:59 +0100

    Fixed indentation, and tags

--- StartPage.php
+++ StartPage.php
@@ -10,60 +10,59 @@ STARTPAGE WEBAPP
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Start Pagina</title>
     </head>
-    <div id="mainContainer">
-        <div id="header">
-            <div id="logo"> <img src="img/stenden.png" alt="NHL_STENDEN"> 
-			</div>
-			<div id="headertxt">
-				<div class="login">
-					<p>Inloggen</p>
-				</div>
-				
-				<div class="person">
-					<ul class="pers">
-						<li class=""><p>Docent</p>
-						<li class=""><p>Student</p>
-					</ul>
-				</div>
-				
-				<div class="language">
-					<ul class="lang">
-						<li class=""><p>NL<p></li>
-						<li class=""><p>EN</p></li>
-					</ul>
-				</div>
-			</div>
-        </div> 
-        <div id="middlebox">
-			<div id="tilecontainer">
-				<div class="calender">
-					
-				</div>
-					
-				<div class="tile">
-				
-				</div>
-				
-				<div class="tile">
-				
-				</div>
-				
-				<div class="tile">
-				
-				</div>
-				
-				<div class="tile">
-				
-				</div>
-				
-				<div class="tile">
-				
-				</div>
-			</div>
-		</div>
-
-
-    </div>
-
-
     <body>
+        <div id="mainContainer">
+            <div id="header">
+                <div id="logo">
+                    <img src="img/stenden.png" alt="NHL_STENDEN"> 
+                </div>
+                <div id="headertxt">
+                    <div class="login">
+                        <p>Inloggen</p>
+                    </div>
+
+                    <div class="person">
+                        <ul class="pers">
+                            <li class=""><p>Docent</p>
+                            <li class=""><p>Student</p>
+                        </ul>
+                    </div>
+
+                    <div class="language">
+                        <ul class="lang">
+                            <li class=""><p>NL<p></li>
+                            <li class=""><p>EN</p></li>
+                        </ul>
+                    </div>
+                </div>
+            </div> 
+            <div id="middlebox">
+                <div id="tilecontainer">
+                        <div class="calender">
+
+                        </div>
+
+                        <div class="tile">
+
+                        </div>
+
+                        <div class="tile">
+
+                        </div>
+
+                        <div class="tile">
+
+                        </div>
+
+                        <div class="tile">
+
+                        </div>
+
+                        <div class="tile">
+
+                        </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

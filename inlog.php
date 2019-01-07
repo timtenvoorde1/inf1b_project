11fcdf6a9ab04c205f9502ad43be5aa5e7f9302e
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2019-01-07 12:12:42 +0100

    Update inlog.php

--- inlog.php
+++ inlog.php
@@ -13,7 +13,6 @@ INLOG WEBAPP
         <title>Inlog Pagina</title>
     </head>
     <body>
-<<<<<<< HEAD
         <div id="mainContainer">
             <div id="header">
                 <div id="logo">
@@ -23,44 +22,20 @@ INLOG WEBAPP
             <div id="inlog"> 
                 <div id="inlogbox">
                     <div id="formbox">
-
-                        <form action=.....php" method="POST">
+                       <h2>Login</h2>
+                       <form action="/ppw/loggedin.php" method="POST">
                             <div class="container">
-                                <h2>Login</h2>
-                                <p><b>Username</b></p>
+                                <p><b>Gebruikersnaam</b></p>
                                 <input type="text" placeholder="Enter Username" name="uname" required>
-                                <p><b>Password</b></p>
+                                <p><b>Wachtwoord</b></p>
                                 <input type="password" placeholder="Enter Password" name="psw" required>
                                 <button type="submit">Login</button>
                             </div>
+                       </form>
                     </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
-</body>
-=======
-        <div id="inlog"> 
-            <div id="inlogbox">
-                <div id="formbox">
-                   <h2>Login</h2>
-                   <form action="/ppw/loggedin.php" method="POST">
-                        <div class="container">
-                            <p><b>Gebruikersnaam</b></p>
-                            <input type="text" placeholder="Enter Username" name="uname" required>
-                            <p><b>Wachtwoord</b></p>
-                            <input type="password" placeholder="Enter Password" name="psw" required>
-                            <button type="submit">Login</button>
-                        </div>
-                   </form>
                 </div>
             </div>
         </div>
     </body>
->>>>>>> 44622d4baae4c8cb41e7f898e211361be23f487c
 </html>
 

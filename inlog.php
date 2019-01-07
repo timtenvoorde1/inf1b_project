263fd94aa5bb64e4fa7c31a40ff9f088cd5ae813
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2019-01-07 12:25:40 +0100

    Added php code to process logins

--- inlog.php
+++ inlog.php
@@ -23,7 +23,7 @@ INLOG WEBAPP
                 <div id="inlogbox">
                     <div id="formbox">
                        <h2>Login</h2>
-                       <form action="/ppw/loggedin.php" method="POST">
+                       <form action="<?php htmlentities($_SERVER['PHP_SELF'])?>" method="POST">
                             <div class="container">
                                 <p><b>Gebruikersnaam</b></p>
                                 <input type="text" placeholder="Enter Username" name="uname" required>
@@ -36,6 +36,51 @@ INLOG WEBAPP
                 </div>
             </div>
         </div>
+        <?php
+           /*
+            * Programmer: Maurice Hoekstra
+            * Date Created: 07-01-2019
+            * Description: Plenair web app login page
+            */
+            require 'DBFuncs.php';
+            if(isset($_POST['submit'])){
+                $DBConnect = mysqli_connect('127.0.0.1', 'root', '');
+                $DBName = 'placeholder';
+                $TableName = 'placeholder';
+                $TableName2= 'placeholder';
+                $Username = $_POST['uname'];
+                $Password = $_POST['psw'];
+                if(TableExistCheck($DBConnect, $DBName, $TableName)){
+                    $Query = "SELECT UserID FROM ".$TableName
+                            ." WHERE UserName = ? AND UserPass = ?;";
+                    
+                    if ($stmt = mysqli_prepare($DBConnect, $Query)) {
+                        mysqli_stmt_bind_param($stmt, 'ss' ,$Username,
+                                $Password);
+                        mysqli_stmt_execute($stmt);
+                        mysqli_stmt_bind_result($stmt, $UserID);
+                        mysqli_stmt_store_result($stmt);
+                        if (mysqli_stmt_num_rows($stmt) == 0) {
+                            echo "<p>Invalid username or password</p>"
+                            . "<p>Do you wish to register?</p><a href='signUp.php'>Register</a>";
+                        }
+                        else{
+                            while(mysqli_stmt_fetch($stmt)){
+                                $_SESSION['userID'] = $UserID;
+                                $_SESSION['loggedin'] = TRUE;
+                                echo "You have been logged in!";
+                                header('Location: '.$_GET['page']);
+                            }
+                        }
+                        mysqli_stmt_close($stmt);
+                    }
+                    else{
+                        DBQueryError($DBConnect);
+                    }
+                }
+                mysqli_close($DBConnect);
+            }
+        ?>
     </body>
 </html>
 

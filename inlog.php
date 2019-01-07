50fafe42ef276c607b633e4f83bf3a0038567bd5
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2019-01-07 11:44:55 +0100

    Fixed indentations, form and language of login page

--- inlog.php
+++ inlog.php
@@ -3,38 +3,31 @@
 INLOG WEBAPP
 17-12-2018
 -->
-<html>
+<html lang="nl">
     <head>
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/inlog.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Start Pagina</title>
+        <title>Inlog Pagina</title>
     </head>
     <body>
         <div id="inlog"> 
             <div id="inlogbox">
-                 <div id="formbox">
-
-                 <h2>Login</h2>
-                    <form action="/ppw/loggedin.php" method="POST">
+                <div id="formbox">
+                   <h2>Login</h2>
+                   <form action="/ppw/loggedin.php" method="POST">
                         <div class="container">
-                            <p><b>Username</b></p>
+                            <p><b>Gebruikersnaam</b></p>
                             <input type="text" placeholder="Enter Username" name="uname" required>
-                            <p><b>Password</b></p>
+                            <p><b>Wachtwoord</b></p>
                             <input type="password" placeholder="Enter Password" name="psw" required>
                             <button type="submit">Login</button>
                         </div>
+                   </form>
                 </div>
-                </form>
-
-
             </div>
-
         </div>
-
-
-
     </body>
 </html>
 

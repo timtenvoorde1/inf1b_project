8b6d4c2bccf22dc03aece7d077144376fefd2209
THIJS-LAPTOP\thijs <thijsvanderwall@live.nl>
2019-01-08 16:47:16 +0100

    Footer + content

--- StartPage.php
+++ StartPage.php
@@ -16,62 +16,81 @@ STARTPAGE WEBAPP
             <div id="header">
                 <div id="logo">
                     <a href="StartPage.php">
-                    <img src="img/stenden.png" alt="NHL_STENDEN"> 
+                        <img src="img/stenden.png" alt="NHL_STENDEN"> 
                     </a>
                 </div>
-				
+
                 <div id="headertxt">
                     <div class="login">
-						<ul>
-							<li><a href="inlog.php" >Inloggen</a></li>
-						<ul>
-                    </div>
-
-                    <div class="person">
                         <ul>
-                            <li class=""><a href="">Docent</a></li>
-                            <li class=""><a href="">Student</a></li>
-                        </ul>
-                    </div>
+                            <li><a href="inlog.php" >Inloggen</a></li>
+                            <ul>
+                                </div>
 
-                    <div class="language">
-                        <ul>
-                            <li class=""><a href="">NL</a></li>
-                            <li class=""><a href="">EN</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div> 
-            <div id="middlebox">
-                <div id="tilecontainer">
-                        <div class="calender">
+                                <div class="person">
+                                    <ul>
+                                        <li class=""><a href="">Docent</a></li>
+                                        <li class=""><a href="">Student</a></li>
+                                    </ul>
+                                </div>
 
-                        </div>
+                                <div class="language">
+                                    <ul>
+                                        <li class=""><a href="">NL</a></li>
+                                        <li class=""><a href="">EN</a></li>
+                                    </ul>
+                                </div>
+                                </div>
+                                </div> 
+                                <div id="middlebox">
+                                    <div id="tilecontainer">
+                                        <div class="calender">
 
-                        <div class="tile">
+                                        </div>
 
-                        </div>
+                                        <div class="tile">
 
-                        <div class="tile">
+                                        </div>
 
-                        </div>
+                                        <div class="tile">
 
-                        <div class="tile">
+                                        </div>
 
-                        </div>
+                                        <div class="tile">
 
-                        <div class="tile">
+                                        </div>
 
-                        </div>
+                                        <div class="tile">
 
-                        <div class="tile">
+                                        </div>
 
-                        </div>
-                </div>
-            </div>
-            <div id="footer" >
-                
-            </div>
-        </div>
+                                        <div class="tile">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="footer" >
+                                    <div id="footercontainer" >
+                                        <div id="footersearch">
+                                            <div id="search">
+                                            <input type="text" placeholder="Zoeken...">
+                                            </div>
+                                        </div>
+                                        <div id="footerinfo">
+                                            <div class="footertext" >
+                                                <p class="textfooter"> Contact </p>
+                                            </div>
+                                            <div class="footertext" >
+                                                <p class="textfooter"> Disclaimer </p>
+                                            </div>
+                                            <div class="footertext" >
+                                                <p class="textfooter"> &copy; NHL Stenden </p>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                                </div>
     </body>
 </html>

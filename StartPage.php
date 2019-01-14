b208c0a5368ad0b22753e45c2b0bec7c946ba1b2
NicktenCaat <43609554+NicktenCaat@users.noreply.github.com>
2019-01-14 13:58:38 +0100

    hover change

--- StartPage.php
+++ StartPage.php
@@ -9,7 +9,7 @@ STARTPAGE WEBAPP
 -->
 <html lang="nl">
     <head>
-        <link rel="stylesheet" type="text/css" href="css/style - kopie.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Start Pagina</title>

d02be59a1e24ad764263bd02ba18290313089600
NicktenCaat <43609554+NicktenCaat@users.noreply.github.com>
2019-01-14 13:43:54 +0100

    input type text edit class/ index.php update

--- index.php
+++ index.php
@@ -28,9 +28,9 @@ INLOG WEBAPP
                        <form action="<?php htmlentities($_SERVER['PHP_SELF'])?>" method="POST">
                             <div class="container">
                                 <p><b>Gebruikersnaam</b></p>
-                                <input type="text" placeholder="Enter Username" name="uname" required>
+                                <input type="text" placeholder="Enter Username" name="uname" required class="unamepsw">
                                 <p><b>Wachtwoord</b></p>
-                                <input type="password" placeholder="Enter Password" name="psw" required>
+                                <input type="password" placeholder="Enter Password" name="psw" required class="unamepsw">
                                 <p>Log in als docent? <input type="checkbox" name="teachlogin"></p>
                                 <input type="submit" name="submit" value="Inloggen">
                             </div>
@@ -131,7 +131,9 @@ INLOG WEBAPP
                             <p class="textfooter"> Contact </p>
                         </div>
                         <div class="footertext" >
+                            <a href="disclaimer.php">
                             <p class="textfooter"> Disclaimer </p>
+                            </a>
                         </div>
                         <div class="footertext" >
                             <p class="textfooter"> &copy; NHL Stenden </p>

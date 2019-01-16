b340f4e2b7d94862dd1dc83318b5a1fa114be930
LAPTOP-YANNIK\Yannik-Eisinga <yannik.eisinga@student.nhlstenden.com>
2019-01-16 21:12:08 +0100

    Header en footer toegevoegf

--- kalender.php
+++ kalender.php
@@ -25,12 +25,47 @@ Front-end Dev.
                         <img src="img/stenden.png" alt="NHL_STENDEN"> 
                     </a>
                 </div>
+                 <div id="headertxt">
+                    <div class="login">
+                        <ul>
+                            <li><a href="index.php" >Uitloggen</a></li>
+                        </ul>
+                    </div>
+                    <div class="language">
+                        <ul>
+                            <li class=""><a href="">NL</a></li>
+                            <li class=""><a href="">EN</a></li>
+                        </ul>
+                    </div>
+                </div> 
             </div>
 
             <div id="middleboxCal" >
                 <iframe src="https://calendar.google.com/calendar/embed?src=1tu1trr2fqthm1n7s4kfpkloh0%40group.calendar.google.com&ctz=Europe%2FAmsterdam" 
                         style="border: 0" width="80%" height="100%" frameborder="0" scrolling="no">
                 </iframe>
+            </div>
+            <div id="footer" >
+                <div id="footercontainer" >
+                    <div id="footersearch">
+                        <div id="search">
+                            <input type="text" placeholder="Zoeken..." class="zoektext">
+                        </div>
+                    </div>
+                    <div id="footerinfo">
+                        <div class="footertext" >
+                            <p class="textfooter"> Contact </p>
+                        </div>
+                        <div class="footertext" >
+                            <a href="disclaimer.php">
+                                <p class="textfooter"> Disclaimer </p>
+                            </a>
+                        </div>
+                        <div class="footertext" >
+                            <p class="textfooter"> &copy; NHL Stenden </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 

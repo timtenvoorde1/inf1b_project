1d7c9a520500801d644c11096a8a89a02394f6ad
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2019-01-17 10:43:51 +0100

    Notulen finished

--- seenotule.php
+++ seenotule.php
@@ -45,31 +45,29 @@
                         $DBConnect = DBHandshake('127.0.0.1', 'root', '');
                         $DBName = 'projectplenair';
                         $TableName = 'notulen';
+                        $ID = filter_var($_GET['notulen'], FILTER_VALIDATE_INT);
                         if(TableExistCheck($DBConnect, $DBName, $TableName) === TRUE){
-                                $Query = "";
-                                if ($stmt = mysqli_prepare($DBConnect, $Query)) {
-                                    if(!mysqli_stmt_execute($stmt)){
-                                        DBQueryError($DBConnect);
+                            $Query = "SELECT Tekst"
+                                    . " FROM ".$TableName
+                                    . " WHERE AgendaNR = ? ;";
+                            
+                            if ($stmt = mysqli_prepare($DBConnect, $Query)) {
+                                mysqli_stmt_bind_param($stmt, 'i', $ID);
+                                if(!mysqli_stmt_execute($stmt)){
+                                    DBQueryError($DBConnect);
+                                }
+                                else{
+                                    mysqli_stmt_bind_result($stmt, $Text);
+                                    mysqli_stmt_store_result($stmt);
+                                    if (mysqli_stmt_num_rows($stmt) == 0) {
+                                        echo "<p>De notule bestaat niet!</p>";
                                     }
                                     else{
-                                        mysqli_stmt_bind_result($stmt, $AgendaNR, $Name,
-                                                                $Year, $Period, $Week);
-                                        mysqli_stmt_store_result($stmt);
-                                        if (mysqli_stmt_num_rows($stmt) == 0) {
-                                            echo "<p>De notule bestaat niet!</p>";
-                                        }
-                                        else{
-                                            while (mysqli_stmt_fetch($stmt)) {
-                                                echo "<tr>
-                                                    <td>".$Year." Periode ".$Period." Week ".$Week."</td>
-                                                    <td>".$Name."</td>
-                                                    <td><a href='seenotule.php?notule=".$AgendaNR."'>Bekijk Notule</a></td>
-                                                    </tr>";
-                                                }
-
-                                        
+                                        while (mysqli_stmt_fetch($stmt)) {
+                                            echo $Text;
+                                        }         
                                     }
-                                
+
                                 }
                                 mysqli_stmt_close($stmt);
                             }
@@ -77,8 +75,44 @@
                                 DBQueryError($DBConnect);
                             }
                         }
-                    }
                     mysqli_close($DBConnect);
+                    }
+                    if(isset($_GET['agenda'])){
+                        $DBConnect = DBHandshake('127.0.0.1', 'root', '');
+                        $DBName = 'projectplenair';
+                        $TableName = 'agenda';
+                        $ID = filter_var($_GET['agenda'], FILTER_VALIDATE_INT);
+                        if(TableExistCheck($DBConnect, $DBName, $TableName) === TRUE){
+                            $Query = "SELECT Tekst"
+                                    . " FROM ".$TableName
+                                    . " WHERE AgendaNR = ? ;";
+                            
+                            if ($stmt = mysqli_prepare($DBConnect, $Query)) {
+                                mysqli_stmt_bind_param($stmt, 'i', $ID);
+                                if(!mysqli_stmt_execute($stmt)){
+                                    DBQueryError($DBConnect);
+                                }
+                                else{
+                                    mysqli_stmt_bind_result($stmt, $Text);
+                                    mysqli_stmt_store_result($stmt);
+                                    if (mysqli_stmt_num_rows($stmt) == 0) {
+                                        echo "<p>De agenda bestaat niet!</p>";
+                                    }
+                                    else{
+                                        while (mysqli_stmt_fetch($stmt)) {
+                                            echo '<p>Bespreekpunten:</p><p>'.$Text.'</p>';
+                                        }         
+                                    }
+
+                                }
+                                mysqli_stmt_close($stmt);
+                            }
+                            else{
+                                DBQueryError($DBConnect);
+                            }
+                        }
+                    mysqli_close($DBConnect);
+                    }
                 ?>
                     
                 </div>

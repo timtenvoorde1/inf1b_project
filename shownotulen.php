8dc52e08620e074447b9f4443ad6b6707b4737f2
DESKTOP-CEPJOH7\twanv <twan.verdel@student.nhlstenden.com>
2019-01-23 19:28:50 +0100

    Notulen is responsive
    Notulen en functies responsive

--- shownotulen.php
+++ shownotulen.php
@@ -9,8 +9,9 @@ if (!isset($_SESSION['loggedin'])) {
     <head>
         <meta charset="UTF-8">
         <title>Notulen</title>
-        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link type="text/css" rel="stylesheet" href="css/notulen.css">
+        <link type="text/css" rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <div id="mainContainer">
